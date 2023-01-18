1e67bc61aa61dfc684c7b129452a46813841ef9a
yhryyq <yhryyq@163.com>
2023-01-17 20:39:07 -0800

    add two statments

--- function.c
+++ function.c
@@ -4,6 +4,7 @@ int addOne(int a,int b)
 {
 	int c = 1;
 	result = a+ b+ c -1+2;
+	result = result + 1;
 	useAPI(result);
 	return result;
 }
@@ -11,5 +12,6 @@ int addOne(int a,int b)
 void useAPI(int x){
 	int y = 2*x;
 	y = y +1;
+	y = y - 2;
 	Py_ABS(y);
 }
