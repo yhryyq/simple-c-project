3c93e525256d65814ab47a16a7f6a9d97106c679
yhryyq <yhryyq@163.com>
2023-01-17 20:40:06 -0800

    change 2 statements

--- function.c
+++ function.c
@@ -4,7 +4,7 @@ int addOne(int a,int b)
 {
 	int c = 1;
 	result = a+ b+ c -1+2;
-	result = result + 1;
+	result = result + 3;
 	useAPI(result);
 	return result;
 }
@@ -12,6 +12,6 @@ int addOne(int a,int b)
 void useAPI(int x){
 	int y = 2*x;
 	y = y +1;
-	y = y - 2;
+	y = y - 4;
 	Py_ABS(y);
 }
